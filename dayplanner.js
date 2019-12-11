45e6f895fe6204645e23d971b2a729a186040ffc
Priya Mahalanobis <pmahalan@knights.ucf.edu>
2019-12-11 17:00:48 -0500

    cleaned up index.html

--- dayplanner.js
+++ dayplanner.js
@@ -1,34 +1,37 @@
+// Aaaaalll our JQuery code is going to go beneath this one line. Closing bracket+parenthesis should be very last things.
 $("document").ready(function(){
-  //update the page with the values from local storage
-  $(".description9").val(localStorage.getItem("hour9"));
 
-(function()
-{
+  (function()
   // instantiate a moment object
-  var NowMoment = moment();
-  // display value of moment object in #displayMoment div
+  {var NowMoment = moment();
+  // display value of moments.js object in #displayMoment div
   var eDisplayMoment = document.getElementById('displayMoment');
   eDisplayMoment.innerHTML = NowMoment.format('MMMM Do YYYY, h:mm:ss a');
-  
   // instantiate a JavaScript Date object
   var NowDate = new Date();
-  
 })();
 
+//NOW ON TO THE LOCAL STORAGE CODE.
 
-//when we click the button, we need to get the value from the day input field.
-$("#savebutton9").on("click", function () {
-  //get the value of the corresponding input.
-  var itemText= $(".description9").val();
-  var itemName = $(".description9").attr("data-item");
-  //log out the item value
-  console.log("Item: ", itemText );
-  //get the id for the name to get it back to put it on the page
-  //get the value of the textarea to set as the value
-  //save to local storage
-  localStorage.setItem(itemName,itemText);
+  //This is what updates the page with the values from local storage!
+  $(".description9").val(localStorage.getItem("hour9"));
+
+  //When the user clicks the save button,
+  $("#savebutton9").on("click", function () {
+    //The thing with the class "description9" is the value (represented by variable itemText),
+    var itemText= $(".description9").val();
+
+    //As well as the attribute of the thing called "data item", represented by variable itemName.
+    var itemName = $(".description9").attr("data-item");
+
+    //We log this out, so that we can inspect the page and see whether or not it works.
+    console.log("Item: ", itemText);
+
+    //this sets itemText (the variable for the input content) and itemName to local storage.
+    localStorage.setItem(itemName,itemText);
 })
 
+//DELETE THE CODE BELOW IN A BIT, BECUASE I'M PRETTY SURE WE DON'TNEED IT 
 $("#savebutton9").on("click", function () {
 
   //trying to get the closest input
