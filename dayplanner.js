1a8aaba122ed13b129856f8d3f54edfe1df35827
Priya Mahalanobis <pmahalan@knights.ucf.edu>
2019-12-10 23:46:13 -0500

    dove into the .js file; created an empty array, used DOM method to dynamically create things

--- dayplanner.js
+++ dayplanner.js
@@ -1,9 +1,26 @@
-var nine = document.querySelector("#9am");
-var ten = document.querySelector("#10am");
-var eleven = document.querySelector("#11am");
-var twelve = document.querySelector("#12pm");
-var one = document.querySelector("#1pm");
-var two = document.querySelector("#2pm");
-var three = document.querySelector("#3pm");
-var four = document.querySelector("#4pm");
-var five = document.querySelector("#5pm");
+var userinput = document.querySelector("#userplans");
+
+userinput.innerHTML = "";
+//clears userinput element
+
+
+var plans = [];
+
+for (var i = 0; i < plans.length; i++) {
+    var customized = plans[i];
+}
+//for each index populated in the array, we have a variable called 'customized'[i]
+
+userinput.textContent = customized;
+//the text content of the user-input fields is going to become the content of the array.
+
+plans.addEventListener('keypress', function (e) {
+    var key = e.which || e.keyCode;
+    if (key === 13) { // 13 is enter
+      // code for enter
+    }
+}
+
+//I want to build something that:
+//Upon the user's typing something in a field and hitting "enter",
+//The stuff they've entered is committed to the local memory. Most likely through an array.
